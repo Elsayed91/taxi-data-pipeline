ee5361323303f76fb5ef85eeb7f245f2b6eead38
Lestrang1991 <liftingthedata@gmail.com>
2023-01-03 15:01:07 +0200

    19a09024d9

--- components/airflow/dags/full_refresh_dag.py
+++ components/airflow/dags/full_refresh_dag.py
@@ -83,3 +83,34 @@ with DAG(
         random_name_postfix_length=2,
         name_prefix="",
     )
+
+    with TaskGroup(group_id="spark-job-full-refresh") as tg1:
+        t1 = SparkKubernetesOperator(
+            task_id="spark-etl",
+            namespace="default",
+            application_file=f"templatess/spark_pod_template.yaml",
+            params={
+                "project": GOOGLE_CLOUD_PROJECT,
+                "image": f"eu.gcr.io/{GOOGLE_CLOUD_PROJECT}/spark",
+                # "mainApplicationFile": "local:///git/repo/components/spark/src/full-refresh-job.py",
+                "mainApplicationFile": "local:///git/repo/components/spark/src/spark_utilities.py",
+                "name": "spark-k8s-init",
+                "instances": 4,
+                "gitsync": True,
+                "nodeSelector": JOBS_NODE_POOL,
+                "env": {
+                    "URI": f"gs://{STAGING_BUCKET}/yellow/*",
+                    "NAME_PREFIX": "yellow_tripdata_"
+                }
+                    
+            },
+        )
+
+    t2 = SparkKubernetesSensor(
+        task_id="spark-etl-monitor",
+        application_name="{{ task_instance.xcom_pull(task_ids='spark-job-full-refresh.spark_full_refresh') ['metadata']['name'] }}",
+        attach_log=True,
+    )
+        t1 >> t2 # type: ignore
+    
+    t1 >> tg1 # type: ignore
