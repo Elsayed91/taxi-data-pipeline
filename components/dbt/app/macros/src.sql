4dc515ba6869df9cf78557264d4fca97b51ed308
Lestrang1991 <liftingthedata@gmail.com>
2023-01-04 09:23:17 +0200

    f9ebac6fea

--- components/dbt/app/macros/src.sql
+++ components/dbt/app/macros/src.sql
@@ -1,7 +1,53 @@
+{% macro ref(model_name)%}
+  {% if target.name == 'test' %}
+    {% if model_name.startswith('seed') %}
+      {% do return(builtins.ref(model_name).include(database=false)) %}
+    {% else %}
+  {% do return(builtins.ref('test_' ~ model_name).include(database=false)) %}
+    {% endif %}
+  {% else %}
+    {% do return(builtins.ref(model_name).include(database=false)) %}
+  {% endif %}
+{% endmacro %}
+
+
+{% macro source(dataset_name, model_name, test_table=None) %}
+  {% if target.name == 'test' %}
+  {% do return(builtins.ref('test_' ~ test_table).include(database=false)) %}
+  {% else %}
+    {{ do return(builtins.source(dataset_name, model_name).include(database=false)) }}
+  {% endif %}
+{% endmacro %}
+
+{# 
+
+{% macro refv2(model_name)%}
+  {% if target.name == 'test' %}
+    {% if model_name.startswith('seed') %}
+      {{ ref(model_name) }}
+    {% else %}
+      {{ source('test_' ~ model_name) }}
+    {% endif %}
+  {% else %}
+    {{ ref(model_name) }}
+  {% endif %}
+{% endmacro %} 
+
+
+{% macro src(dataset_name, model_name) %}
+  {% if target.name == 'test' %}
+    {{ ref('test_'|string + this.name) }}
+  {% else %}
+    {{ source(dataset_name, model_name) }}
+  {% endif %}
+{% endmacro %}
+
+
+
 {% set test_dataset = env_var('UNIT_TESTS_DATASET') | string() %}
 
 
-{% macro refv2(model_name)%}
+{# {% macro refv2(model_name)%}
   {% if target.name == 'test' %}
     {% if model_name.startswith('seed') %}
       {{ ref(model_name) }}
@@ -24,54 +70,5 @@
   {% else %}
     {{ source(dataset_name, model_name) }}
   {% endif %}
-{% endmacro %}
-
-
-
-{# 
-{% macro ref_for_test(model_name) %}
- 
-      {%- set normal_ref_relation = ref(model_name) -%}
-      {%- set test_ref_relation = this -%}
-
-      {% if target.name == 'test' %}
-
-            {%- set test_ref = adapter.get_relation(
-                  database = test_ref_relation.database,
-                  schema = test_dataset,
-                  identifier = 'test_' ~ test_ref_relation.identifier) 
-            -%}
-            
-            {{ return(test_ref) }}
-
-      {% else %}
-      
-            {{ return(normal_ref_relation) }}
-      
-      {% endif %}
- 
-{% endmacro %}
-
-
-{% macro source_for_test(source_schema, source_name) %}
-
-      {%- set normal_source_relation = source(source_schema, source_name) -%}
-      {%- set test_ref_relation = this -%}
-
-      {% if target.name == 'test' %}
-
-            {%- set test_source_relation = adapter.get_relation(
-                  database = test_ref_relation.database,
-                  schema = test_dataset,
-                  identifier = 'test_' ~ test_ref_relation.identifier) 
-            -%}
-            
-            {{ return(test_source_relation) }}
-
-      {% else %}
-
-            {{ return(normal_source_relation) }}
-
-      {% endif %}
-
 {% endmacro %} #}
+ #}
