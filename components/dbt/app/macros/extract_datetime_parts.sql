54edd2a5e8979e443de80642c98ba60b49fc00c3
Lestrang1991 <liftingthedata@gmail.com>
2023-01-04 04:01:37 +0200

    4523cf04d7

--- components/dbt/app/macros/extract_datetime_parts.sql
+++ components/dbt/app/macros/extract_datetime_parts.sql
@@ -1,7 +1,7 @@
 {% macro extract_datetime_parts(timestamp_column) %}
-    extract(day from {{ timestamp_column }}) as day_of_month,
-    extract(month from {{ timestamp_column }}) as month,
-    extract(year from {{ timestamp_column }}) as year,
-    extract(dow from {{ timestamp_column }}) as day_of_week,
-    extract(hour from {{ timestamp_column }}) as hour_of_day
+    extract(DAY from {{ timestamp_column }}) as day_of_month,
+    extract(MONTH from {{ timestamp_column }}) as month,
+    extract(YEAR from {{ timestamp_column }}) as year,
+    extract(DAYOFWEEK from {{ timestamp_column }}) as day_of_week,
+    extract(HOUR from {{ timestamp_column }}) as hour_of_day
 {% endmacro %}
