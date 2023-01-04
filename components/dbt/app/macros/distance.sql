d7bb349c5d85eda8ed5ebb067cf6d6f4cb36d043
Lestrang1991 <liftingthedata@gmail.com>
2023-01-04 03:49:50 +0200

    7a91e22d93

--- components/dbt/app/macros/distance.sql
+++ components/dbt/app/macros/distance.sql
@@ -4,9 +4,9 @@
 
 
 {% macro distances_from_airports(lon1, lat1, lon2, lat2) -%}
-    pickup_jfk_distance: ST_DISTANCE(ST_GEOGPOINT(-73.778889, 40.639722), ST_GEOGPOINT({{lon1}}, {{lat1}}))
-    dropoff_jfk_distance: ST_DISTANCE(ST_GEOGPOINT(-73.778889, 40.639722), ST_GEOGPOINT({{lon2}},{{lat2}}))
-    pickup_ewr_distance: ST_DISTANCE(ST_GEOGPOINT(-74.168611, 40.6925), ST_GEOGPOINT({{lon1}}, {{lat1}}))
-    dropoff_ewr_distance: ST_DISTANCE(ST_GEOGPOINT(-74.168611, 40.6925), ST_GEOGPOINT({{lon2}},{{lat2}}))
+    pickup_jfk_distance: ST_DISTANCE(ST_GEOGPOINT(-73.778889, 40.639722), ST_GEOGPOINT({{lon1}}, {{lat1}})),
+    dropoff_jfk_distance: ST_DISTANCE(ST_GEOGPOINT(-73.778889, 40.639722), ST_GEOGPOINT({{lon2}},{{lat2}})),
+    pickup_ewr_distance: ST_DISTANCE(ST_GEOGPOINT(-74.168611, 40.6925), ST_GEOGPOINT({{lon1}}, {{lat1}})),
+    dropoff_ewr_distance: ST_DISTANCE(ST_GEOGPOINT(-74.168611, 40.6925), ST_GEOGPOINT({{lon2}},{{lat2}})),
     dropoff_lga_distance: ST_DISTANCE(ST_GEOGPOINT(-73.872611, 40.77725), ST_GEOGPOINT({{lon2}},{{lat2}}))
 {%- endmacro %}
