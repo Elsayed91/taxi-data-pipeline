0ee0381073a64c16400197dbe5f51ac6a50cb772
Lestrang1991 <liftingthedata@gmail.com>
2023-01-04 08:08:53 +0200

    6ad892afd6

--- components/dbt/app/macros/src.sql
+++ components/dbt/app/macros/src.sql
@@ -19,20 +19,43 @@
 {% macro ref(model_name)%}
   {% if target.name == 'test' %}
     {% if model_name.startswith('seed') %}
-      {{ builtins.ref(model_name) }}
+      {{ builtins.ref(model_name).include(database=false) }}
     {% else %}
-      {{ builtins.ref('test_' ~ model_name) }}
+      {{ builtins.ref('test_' ~ model_name).include(database=false) }}
     {% endif %}
   {% else %}
-    {{ builtins.ref(model_name) }}
+    {{ builtins.ref(model_name).include(database=false) }}
   {% endif %}
 {% endmacro %}
 
 
 {% macro source(dataset_name, model_name) %}
   {% if target.name == 'test' %}
-    {{ builtins.ref('test_'|string + this.name) }}
+    {{ builtins.ref('test_'|string + this.name).include(database=false) }}
   {% else %}
     {{ builtins.source(dataset_name, model_name) }}
   {% endif %}
 {% endmacro %}
+
+
+{# {% macro refv2(model_name)%}
+  {% if target.name == 'test' %}
+    {% if model_name.startswith('seed') %}
+      {{ ref(model_name) }}
+    {% else %}
+      {{ ref('test_' ~ model_name) }}
+    {% endif %}
+  {% else %}
+    {{ ref(model_name) }}
+  {% endif %}
+{% endmacro %}
+
+
+{% macro src(dataset_name, model_name) %}
+  {% if target.name == 'test' %}
+    {{ ref('test_'|string + this.name) }}
+  {% else %}
+    {{ source(dataset_name, model_name) }}
+  {% endif %}
+{% endmacro %}
+ #}
