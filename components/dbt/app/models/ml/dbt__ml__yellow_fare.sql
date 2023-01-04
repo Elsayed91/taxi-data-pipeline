803f848c960c54571fb22ad9b096eb05f0f25e82
Lestrang1991 <liftingthedata@gmail.com>
2023-01-04 03:56:40 +0200

    0e8d7f7ff7

--- components/dbt/app/models/ml/dbt__ml__yellow_fare.sql
+++ components/dbt/app/models/ml/dbt__ml__yellow_fare.sql
@@ -21,8 +21,8 @@ select
         )
     }},
 from {{ source("staging_data", "yellow_staging") }} bt
-left join {{ ref("seed_zones") }} z1 on bt.pulocationid = z1.location_id
-left join {{ ref("seed_zones") }} z2 on bt.dolocationid = z2.location_id
+left join {{ ref("seed_zones") }} z1 on bt.pulocationid = z1.LocationID
+left join {{ ref("seed_zones") }} z2 on bt.dolocationid = z2.LocationID
 
 {% if is_incremental() %}
 {% set incremental_date = env_var('RUN_DATE') %}
