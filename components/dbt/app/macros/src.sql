8db034b09a7789f23a91ea4485227e1ff9813149
Lestrang1991 <liftingthedata@gmail.com>
2023-01-04 09:46:27 +0200

    209aa70698

--- components/dbt/app/macros/src.sql
+++ components/dbt/app/macros/src.sql
@@ -11,7 +11,7 @@
 {% endmacro %}
 
 
-{% macro src(dataset_name, model_name, test_table=None) %}
+{% macro source(dataset_name, model_name, test_table=None) %}
   {% if target.name == 'test' and test_table is not none and test_table.startswith('dbt__') %}
     {% do return(builtins.ref('test_' ~ test_table).include(database=false)) %}
   {% else %}
