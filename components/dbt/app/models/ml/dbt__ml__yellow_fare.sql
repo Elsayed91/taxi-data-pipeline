1167f6a0349d2bd08b7a91af90178cfd0d6af2c3
Lestrang1991 <liftingthedata@gmail.com>
2023-01-04 04:30:48 +0200

    2b4017d01a

--- components/dbt/app/models/ml/dbt__ml__yellow_fare.sql
+++ components/dbt/app/models/ml/dbt__ml__yellow_fare.sql
@@ -17,8 +17,7 @@ select
     DATE(bt.tpep_pickup_datetime) as date,
     TIMESTAMP_DIFF(bt.tpep_dropoff_datetime, bt.tpep_pickup_datetime, MINUTE) AS trip_duration,
     {{ extract_datetime_parts("bt.tpep_pickup_datetime") }},
-    {{ distance("z1.longitude", "z1.latitude", "z2.longitude", "z2.latitude") }}
-    as geo_distance,
+    {{ distance("z1.longitude", "z1.latitude", "z2.longitude", "z2.latitude") }} as geo_distance,
     {{
         distances_from_airports(
             "z1.longitude", "z1.latitude", "z2.longitude", "z2.latitude"
@@ -29,7 +28,8 @@ left join {{ ref("seed_zones") }} z1 on bt.pulocationid = z1.LocationID
 left join {{ ref("seed_zones") }} z2 on bt.dolocationid = z2.LocationID 
 
 {% if is_incremental() %}
-{% set incremental_date = env_var('RUN_DATE') %}
+{% set incremental_date = env_var('RUN_DATE', default=datetime.now().strftime('%Y-%m-%d')) %}
 WHERE
        DATE(bt.tpep_pickup_datetime) >= date_parse('%Y-%m-%d', "{{ incremental_date }}")
 {% endif %}
+
