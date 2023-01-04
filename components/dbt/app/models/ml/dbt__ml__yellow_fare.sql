0241e6fba49459aefb3b048299899e9e2af1a804
Lestrang1991 <liftingthedata@gmail.com>
2023-01-04 07:41:53 +0200

    fad55a8d43

--- components/dbt/app/models/ml/dbt__ml__yellow_fare.sql
+++ components/dbt/app/models/ml/dbt__ml__yellow_fare.sql
@@ -1,7 +1,7 @@
-
+{% set ML_DATASET = {{ env_var('ML_DATASET') }} %}
 {{ config(
         materialized = 'incremental',
-        dataset = "{{ env_var('ML_DATASET') }}",
+        dataset = {{ ML_DATASET }},
         partition_expiration_days = 180,
         partition_by = {'field': 'date', 'data_type': 'date',  "granularity": "month"},
 ) }}
