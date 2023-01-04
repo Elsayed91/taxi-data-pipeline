bae1303f931317201f53548de96b16ded2ad6b2a
Lestrang1991 <liftingthedata@gmail.com>
2023-01-04 05:10:02 +0200

    2ae878cad5

--- components/airflow/dags/scripts/dbt_run.sh
+++ components/airflow/dags/scripts/dbt_run.sh
@@ -58,7 +58,7 @@ if [[ -n $generate_docs ]]; then
     echo "generating elementary report."
     edr monitor send-report --profiles-dir ${DBT_PROFILES_DIR} \
         --google-service-account-path $KEYFILE \
-        --gcs-bucket-name $DOCS_BUCKET \
+        --gcs-bucket-name gs://$DOCS_BUCKET \
         --update-bucket-website true \
         --bucket-file-path elementary/elementary.html
 fi
