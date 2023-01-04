86435ca8aec957f3f556b963b8be5bd136646d56
Lestrang1991 <liftingthedata@gmail.com>
2023-01-04 08:30:15 +0200

    1edddfc227

--- components/dbt/app/macros/src.sql
+++ components/dbt/app/macros/src.sql
@@ -1,19 +1,21 @@
+{% set test_dataset = env_var('UNIT_TESTS_DATASET') %}
+
 {% macro refv2(model_name)%}
   {% if target.name == 'test' %}
     {% if model_name.startswith('seed') %}
       {{ ref(model_name) }}
     {% else %}
-      {{ ref('test_' ~ model_name) }}
+      {{ source(test_dataset, 'test_' ~ model_name) }}
     {% endif %}
   {% else %}
     {{ ref(model_name) }}
   {% endif %}
-{% endmacro %} #}
+{% endmacro %} 
 
 
 {% macro src(dataset_name, model_name) %}
   {% if target.name == 'test' %}
-    {{ ref('test_'|string + this.name) }}
+    {{ source(test_dataset, 'test_'|string + this.name) }}
   {% else %}
     {{ source(dataset_name, model_name) }}
   {% endif %}
