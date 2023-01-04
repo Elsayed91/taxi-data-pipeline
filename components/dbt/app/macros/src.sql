65f3d131eb506626554312ccbb6e48de02fc2332
Lestrang1991 <liftingthedata@gmail.com>
2023-01-04 09:45:26 +0200

    31311f2560

--- components/dbt/app/macros/src.sql
+++ components/dbt/app/macros/src.sql
@@ -12,7 +12,7 @@
 
 
 {% macro src(dataset_name, model_name, test_table=None) %}
-  {% if target.name == 'test' and if test_table is not none and test_table.startswith('dbt__') %}
+  {% if target.name == 'test' and test_table is not none and test_table.startswith('dbt__') %}
     {% do return(builtins.ref('test_' ~ test_table).include(database=false)) %}
   {% else %}
     {% do return(builtins.source(dataset_name, model_name).include(database=false))  %}
