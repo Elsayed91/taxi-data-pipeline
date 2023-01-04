bc43f9ec79e069306410b9a1d76118ebad2d923a
Lestrang1991 <liftingthedata@gmail.com>
2023-01-04 05:17:08 +0200

    13940f9b4d

--- components/airflow/dags/scripts/dbt_run.sh
+++ components/airflow/dags/scripts/dbt_run.sh
@@ -56,11 +56,12 @@ if [[ -n $generate_docs ]]; then
     # echo "uploading dbt docs static HTML to GCS"
     # python ${SCRIPT_DIR}/upload_dbt_results.py
     echo "generating elementary report."
-    edr monitor send-report --profiles-dir ${DBT_PROFILES_DIR} \
-        --google-service-account-path $KEYFILE \
-        --gcs-bucket-name gs://$DOCS_BUCKET \
-        --update-bucket-website true \
-        --bucket-file-path elementary/elementary.html
+    # edr monitor send-report --profiles-dir ${DBT_PROFILES_DIR} \
+    #     --google-service-account-path $KEYFILE \
+    #     --gcs-bucket-name gs://$DOCS_BUCKET \
+    #     --update-bucket-website true \
+    #     --bucket-file-path elementary/elementary.html
+    edr monitor send-report --config-dir .
 fi
 
 if [[ -n $debug ]]; then
