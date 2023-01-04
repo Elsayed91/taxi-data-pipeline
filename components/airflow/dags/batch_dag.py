6c27ba2fc9af875f1d304fbf20b00d1f04913904
Lestrang1991 <liftingthedata@gmail.com>
2023-01-04 21:30:16 +0200

    035052ba1f

--- components/airflow/dags/batch_dag.py
+++ components/airflow/dags/batch_dag.py
@@ -1,11 +1,9 @@
 from airflow import DAG
-import os
 from airflow.operators.python import PythonOperator
 from airflow.utils.dates import days_ago
 from airflow_kubernetes_job_operator.kubernetes_job_operator import (
     KubernetesJobOperator,
 )
-
 # default_args are the default arguments for the DAG
 default_args = {
     "owner": "me",
@@ -14,37 +12,37 @@ default_args = {
 }
 
 # Create a DAG with default_args
-with DAG(
-    dag_id="batch-dag",
-    schedule_interval=None,
+dag = DAG(
+    "batch-dag",
     default_args=default_args,
-    catchup=False,
-    tags=["batch-dag"],
+    description="A dummy DAG to demonstrate command line configuration",
     template_searchpath=["/git/repo/components/airflow/dags"],
-) as dag:
-    GKE_CLUSTER_NAME = os.getenv("GKE_CLUSTER_NAME")
-    GOOGLE_CLOUD_PROJECT = os.getenv("GOOGLE_CLOUD_PROJECT")
-    STAGING_BUCKET = os.getenv("STAGING_BUCKET")
-    BASE = "/git/repo/components"
-    TEMPLATES_PATH = f"{BASE}/airflow/dags/templates"
-    SCRIPTS_PATH = f"{BASE}/airflow/dags/scripts"
-    JOBS_NODE_POOL = os.getenv("JOBS_NODE_POOL")  # remove z after terraform re
-    BASE_NODE_POOL = os.getenv("BASE_NODE_POOL")
+)
+    
+import os
+GKE_CLUSTER_NAME = os.getenv("GKE_CLUSTER_NAME")
+GOOGLE_CLOUD_PROJECT = os.getenv("GOOGLE_CLOUD_PROJECT")
+STAGING_BUCKET = os.getenv("STAGING_BUCKET")
+BASE = "/git/repo/components"
+TEMPLATES_PATH = f"{BASE}/airflow/dags/templates"
+SCRIPTS_PATH = f"{BASE}/airflow/dags/scripts"
+JOBS_NODE_POOL = os.getenv("JOBS_NODE_POOL")  # remove z after terraform re
+BASE_NODE_POOL = os.getenv("BASE_NODE_POOL")
 
-    t4 = KubernetesJobOperator(
-        task_id="resize",
-        body_filepath=f"{TEMPLATES_PATH}/pod_template.yaml",
-        command=["/bin/bash", "-c"],
-        arguments=["echo", "{{ dag_run.conf.URI }}", "clusters;", "echo $dag_uri"],
-        jinja_job_args={
-            "image": "google/cloud-sdk:alpine",
-            "name": "testi",
-        },
-        envs={"dag_uri": "{{ dag_run.conf.URI }}"},
-        in_cluster=True,
-        random_name_postfix_length=2,
-        name_prefix="",
-    )
+t4 = KubernetesJobOperator(
+    task_id="resize",
+    body_filepath=f"{TEMPLATES_PATH}/pod_template.yaml",
+    command=["/bin/bash", "-c"],
+    arguments=["echo", "{{ dag_run.conf.URI }}", "clusters;", "echo $dag_uri"],
+    jinja_job_args={
+        "image": "google/cloud-sdk:alpine",
+        "name": "testi",
+    },
+    envs={"dag_uri": "{{ dag_run.conf.URI }}"},
+    in_cluster=True,
+    random_name_postfix_length=2,
+    name_prefix="",
+
 # Define a function that prints the command line configuration
 # def print_conf(**kwargs):
 #     conf = kwargs["dag_run"].conf
