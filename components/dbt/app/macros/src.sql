11eae7de8d6ee13c771b6be4133c88dc6843d24b
Lestrang1991 <liftingthedata@gmail.com>
2023-01-04 07:31:11 +0200

    7781c030e1

--- components/dbt/app/macros/src.sql
+++ components/dbt/app/macros/src.sql
@@ -1,21 +1,3 @@
-{# {% macro src(func, model_name, dataset_name=None) %}
-  {% if target.name == 'test' %}
-    {% if model_name.startswith('seed_') %}
-      {{ ref(model_name) }}
-    {% elif func == 'ref' %}
-      {{ ref('test_' ~ model_name) }}
-    {% else %}
-      {{ ref('test_'|string + this.name) }}
-    {% endif %}
-  {% else %}
-    {% if func == 'ref' %}
-      {{ ref(model_name) }}
-    {% else %}
-      {{ source(dataset_name, model_name) }}
-    {% endif %}
-  {% endif %}
-{% endmacro %} #}
-
 {% macro ref(model_name)%}
   {% if target.name == 'test' %}
     {% if model_name.startswith('seed') %}
