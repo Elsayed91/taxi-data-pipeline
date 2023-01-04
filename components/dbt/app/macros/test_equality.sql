d6e536fee5c8121a2a99aa5412298ba02c5fe099
Lestrang1991 <liftingthedata@gmail.com>
2023-01-04 10:37:57 +0200

    ed6d6e5366

--- components/dbt/app/macros/test_equality.sql
+++ components/dbt/app/macros/test_equality.sql
@@ -4,7 +4,7 @@
 
 
 {%- if target.name == 'test' -%}
-{% set model = builtins.ref('test_' ~ model_name).include(database=false) %}
+{% set model = builtins.ref('test_' ~ model).include(database=false) %}
 select count(*) from ((select * from {{ model }} except select * from {{ compare_model }} )  union (select * from {{ compare_model }} except select * from {{ model }} )) tmp
 
 {%- else -%}
