cb5a3d323a048f83828bbae8c8b9014617a758f4
Lestrang1991 <liftingthedata@gmail.com>
2023-01-05 13:39:21 +0200

    40ac35c480

--- components/lambda/main.py
+++ components/lambda/main.py
@@ -61,7 +61,7 @@ def get_credentials(secret_id: str = "gcp_key"):
     )
     secret_value = json.loads(get_secret_value_response["SecretString"])
 
-    key = decodebytes(secret_value["privateKeyData"].encode())
+    key = decodebytes(secret_value["private_key"].encode())
     key = json.loads(key)  # type: ignore
 
     logger.info(key)
