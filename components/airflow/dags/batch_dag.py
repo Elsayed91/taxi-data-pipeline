31c158ef254e194f5767267941c55096f1ee7402
Lestrang1991 <liftingthedata@gmail.com>
2023-01-05 09:20:50 +0200

    a7d5e7c69d

--- components/airflow/dags/batch_dag.py
+++ components/airflow/dags/batch_dag.py
@@ -4,6 +4,9 @@ from airflow.utils.dates import days_ago
 from airflow_kubernetes_job_operator.kubernetes_job_operator import (
     KubernetesJobOperator,
 )
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+from datetime import datetime, timedelta
 
 # default_args are the default arguments for the DAG
 default_args = {
@@ -31,35 +34,45 @@ with DAG(
     JOBS_NODE_POOL = os.getenv("JOBS_NODE_POOL")  # remove z after terraform re
     BASE_NODE_POOL = os.getenv("BASE_NODE_POOL")
 
-    t4 = KubernetesJobOperator(
-        task_id="resize",
-        body_filepath=f"{TEMPLATES_PATH}/pod_template.yaml",
-        command=["/bin/bash", "-c"],
-        arguments=["echo", "{{ dag_run.conf.URI }}", "clusters;", "echo $dag_uri"],
-        jinja_job_args={
-            "image": "google/cloud-sdk:alpine",
-            "name": "testi",
-        },
-        envs={"dag_uri": "{{ dag_run.conf.URI }}"},
-        in_cluster=True,
-        random_name_postfix_length=2,
-        name_prefix="",
-        dag=dag,
-    )
+    # t4 = KubernetesJobOperator(
+    #     task_id="resize",
+    #     body_filepath=f"{TEMPLATES_PATH}/pod_template.yaml",
+    #     command=["/bin/bash", "-c"],
+    #     arguments=["echo", "{{ dag_run.conf.URI }}", "clusters;", "echo $dag_uri"],
+    #     jinja_job_args={
+    #         "image": "google/cloud-sdk:alpine",
+    #         "name": "testi",
+    #     },
+    #     envs={"dag_uri": "{{ dag_run.conf.URI }}"},
+    #     in_cluster=True,
+    #     random_name_postfix_length=2,
+    #     name_prefix="",
+    #     dag=dag,
+    # )
 
-    def print_conf(**kwargs):
-        filename = "{{ dag_run.conf['URI'] }}".split("/")[-1]
-        print(f"filename is {filename}")
-        return filename
+    def my_dag_func(**kwargs):
+        conf = kwargs["dag_run"].conf
+        file_uri = conf["URI"]
+        bucket, file_path = file_uri.replace("s3://", "").split("/", 1)
+        file_name = file_path.split("/")[-1]
+        return bucket, file_path, file_name
 
     # Create a PythonOperator that calls the print_conf function
     print_conf_task = PythonOperator(
         task_id="print_conf",
-        python_callable=print_conf,
+        python_callable=my_dag_func,
         provide_context=True,
     )
 
-    t4 >> print_conf_task
+    task = BashOperator(
+        task_id="bash_operator_task",
+        bash_command='echo "Bucket: {{ task_instance.xcom_pull(task_ids="print_conf", key="bucket") }}, \
+        File Path: {{ task_instance.xcom_pull(task_ids="print_conf", key="file_path") }}',
+    )
+    # Pass in the variables using the `provide_context` parameter
+    # provide_context=True
+
+    print_conf_task >> task
 # Set the order of the tasks using set_upstream and set_downstream
 # print_conf_task
 
