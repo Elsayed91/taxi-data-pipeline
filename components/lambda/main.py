b3405c901c1328e26a7ec0a7527fa0d27576f72e
Lestrang1991 <liftingthedata@gmail.com>
2023-01-05 13:32:55 +0200

    d4ed671cd4

--- components/lambda/main.py
+++ components/lambda/main.py
@@ -37,7 +37,7 @@ import googleapiclient.discovery
 import kubernetes
 from kubernetes.stream import stream
 import urllib.parse
-from google.oauth2.service_account import Credentials
+from google.oauth2 import service_account
 from aws_lambda_typing.context import Context as LambdaContext
 import boto3
 from base64 import decodebytes, encode
@@ -59,13 +59,29 @@ def get_credentials(secret_id: str = "gcp_key"):
     get_secret_value_response = secrets_manager_client.get_secret_value(
         SecretId=secret_id
     )
-    key_file = json.loads(get_secret_value_response["SecretString"])
+    secret_value = get_secret_value_response["SecretString"]
 
-    with NamedTemporaryFile(mode="w", suffix=".json") as f:
-        json.dump(key_file, f)
-        credentials = Credentials.from_service_account_file(f.name)
-        f.flush()
-    return credentials
+    key = decodebytes(secret_value["privateKeyData"].encode())
+    key = json.loads(key)  # type: ignore
+
+    logger.info(key)
+    # key_file = json.loads(secret_value)
+    # logger.info(f"jsonkey {key_file}")
+    # key_file = json.loads(get_secret_value_response["SecretString"])
+
+    # with NamedTemporaryFile(mode="w", suffix=".json") as f:
+    #     json.dump(key_file, f)
+    #     credentials = Credentials.from_service_account_file(f.name)
+    #     f.flush()
+    # return credentials
+    # with NamedTemporaryFile(mode="w", suffix=".json", delete=False) as f:
+    #     json.dump(key_file, f)
+    #     f.flush()
+    # credentials = service_account.Credentials.from_service_account_file(
+    #     f.name
+    # )  # Load the credentials from the temporary file
+    # os.unlink(f.name)  # Delete the temporary file
+    return service_account.Credentials.from_service_account_info(key)
 
 
 def lambda_handler(event: dict, context: LambdaContext) -> None:
