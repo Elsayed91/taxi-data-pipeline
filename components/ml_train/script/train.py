d20f460f9c53a483a82bd1d03ba6f3dd35b63bf0
Lestrang1991 <liftingthedata@gmail.com>
2023-01-08 20:53:41 +0200

    046cd4c1b3

--- components/ml_train/script/train.py
+++ components/ml_train/script/train.py
@@ -12,12 +12,7 @@ import logging
 
 # http://michael-harmon.com/blog/GreenBuildings3.html
 logger = logging.getLogger(__name__)
-logger.setLevel(logging.DEBUG)
-ch = logging.StreamHandler()
-ch.setLevel(logging.DEBUG)
-formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
-ch.setFormatter(formatter)
-logger.addHandler(ch)
+
 
 if __name__ == "__main__":
     # define environmental variables
