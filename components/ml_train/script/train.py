7bcb139e4c250c6605c9423c1663933985aea49d
Lestrang1991 <liftingthedata@gmail.com>
2023-01-08 22:02:51 +0200

    0b4c153cae

--- components/ml_train/script/train.py
+++ components/ml_train/script/train.py
@@ -32,9 +32,8 @@ model_name = "xgboost-fare-predictor"
 
 exp = mlflow.set_experiment(mlflow_experiment_name)
 exp_id = exp.experiment_id
-print(exp_id)
 df = load_data(target_dataset, target_table, 10)  # type: ignore
-print("splitting data")
+print(f"df shape is {df.shape}")
 y = df[target_column]
 
 X = df.drop([target_column], axis=1)
