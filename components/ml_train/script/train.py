f215f8d515db12e5d5cf0f33574347773ce22e83
Lestrang1991 <liftingthedata@gmail.com>
2023-01-08 22:54:54 +0200

    8693443cb4

--- components/ml_train/script/train.py
+++ components/ml_train/script/train.py
@@ -31,7 +31,7 @@ model_name = "xgboost-fare-predictor"
 
 exp = mlflow.set_experiment(mlflow_experiment_name)
 exp_id = exp.experiment_id
-df = load_data(target_dataset, target_table, 15)  # type: ignore
+df = load_data(target_dataset, target_table, "21")  # type: ignore
 print(f"df shape is {df.shape}")
 y = df[target_column]
 X = df.drop([target_column], axis=1)
