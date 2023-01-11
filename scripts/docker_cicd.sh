0caab04b5c07d1cb0a8a10b865d57746084c4a7b
Lestrang1991 <liftingthedata@gmail.com>
2023-01-12 01:38:52 +0200

    55513c4948

--- scripts/docker_cicd.sh
+++ scripts/docker_cicd.sh
@@ -9,23 +9,20 @@ gcloud container clusters get-credentials $GKE_CLUSTER_NAME --project=$PROJECT -
 # Loop through the changed components and build their images
 for component in "${CHANGED_COMPONENTS[@]}"; do
   echo "Building image for $component..."
-  cd ${PWD}/components/$component/docker
-  echo $PWD
-  ls
-  cat Dockerfile
-  # Build cloudbuild.yaml for each component
-  cat >cloudbuild.yaml <<EOL
-steps:
-  - id: "$component"
-    name: "gcr.io/kaniko-project/executor:latest"
-    args:
-      [
-        "--context=.",
-        "--cache=true",
-        "--cache-ttl=6h",
-        "--destination=eu.gcr.io/${PROJECT}/$component",
-      ]
-EOL
+  #   cd ${PWD}/components/$component/docker
+  #   # Build cloudbuild.yaml for each component
+  #   cat >cloudbuild.yaml <<EOL
+  # steps:
+  #   - id: "$component"
+  #     name: "gcr.io/kaniko-project/executor:latest"
+  #     args:
+  #       [
+  #         "--context=.",
+  #         "--cache=true",
+  #         "--cache-ttl=6h",
+  #         "--destination=eu.gcr.io/${PROJECT}/$component",
+  #       ]
+  # EOL
 
   # Submit the build to Google Cloud Build
   gcloud builds submit .
@@ -35,5 +32,7 @@ EOL
     echo "Rolling out deployment for $component..."
     kubectl rollout restart deployment $component
     echo "Deployment for $component rolled out."
+  else
+    echo "No Attached Kubernetes Deployment to restart."
   fi
 done
