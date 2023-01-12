dcaa45053e648d5051aa526907b8843d0b40c195
Lestrang1991 <liftingthedata@gmail.com>
2023-01-12 02:15:45 +0200

    7d6b527cda

--- .github/workflows/components_cicd.sh
+++ .github/workflows/components_cicd.sh
@@ -28,7 +28,7 @@ EOL
   gcloud builds submit .
   echo "Image for $component built and pushed to registry."
 
-  if [[ -d ${PWD}/components/$component/manifests ]]; then
+  if [[ $(find ${PWD}/components/$component/manifests -name "*_deployment.yaml" | wc -l) -gt 0 ]]; then
     echo "Rolling out deployment for $component..."
     kubectl rollout restart deployment $component
     echo "Deployment for $component rolled out."
