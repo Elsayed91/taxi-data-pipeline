66a3f139d4d09ad3c36f62c2535a33f74d9e8063
Lestrang1991 <liftingthedata@gmail.com>
2023-01-12 02:03:45 +0200

    9edfa28081

--- .github/workflows/components-cicd.sh
+++ .github/workflows/components-cicd.sh
@@ -7,32 +7,37 @@ gcloud components install gke-gcloud-auth-plugin
 gcloud container clusters get-credentials $GKE_CLUSTER_NAME --project=$PROJECT --region=$GCP_ZONE
 
 # Loop through the changed components and build their images
-for component in "${CHANGED_COMPONENTS[@]}"; do
+for component in "${CHANGED_DOCKER_COMPONENTS[@]}"; do
   echo "Building image for $component..."
-  #   cd ${PWD}/components/$component/docker
-  #   # Build cloudbuild.yaml for each component
-  #   cat >cloudbuild.yaml <<EOL
-  # steps:
-  #   - id: "$component"
-  #     name: "gcr.io/kaniko-project/executor:latest"
-  #     args:
-  #       [
-  #         "--context=.",
-  #         "--cache=true",
-  #         "--cache-ttl=6h",
-  #         "--destination=eu.gcr.io/${PROJECT}/$component",
-  #       ]
-  # EOL
+  cd ${PWD}/components/$component/docker
+  # Build cloudbuild.yaml for each component
+  cat >cloudbuild.yaml <<EOL
+steps:
+  - id: "$component"
+    name: "gcr.io/kaniko-project/executor:latest"
+    args:
+      [
+        "--context=.",
+        "--cache=true",
+        "--cache-ttl=6h",
+        "--destination=eu.gcr.io/${PROJECT}/$component",
+      ]
+EOL
 
   # Submit the build to Google Cloud Build
-  # gcloud builds submit .
-
+  gcloud builds submit .
   echo "Image for $component built and pushed to registry."
+
   if [[ -d ${PWD}/components/$component/manifests ]]; then
     echo "Rolling out deployment for $component..."
     kubectl rollout restart deployment $component
     echo "Deployment for $component rolled out."
   else
-    echo "No Attached Kubernetes Deployment to restart."
+    echo "No Attached Kubernetes Deployment to restart. "
   fi
+done
+
+for component in "${CHANGED_K8S_COMPONENTS[@]}"; do
+  echo $component
+
 done
