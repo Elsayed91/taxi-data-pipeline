7f4c81a4ffd07597a2956ac8f7a391b2469dbae7
Lestrang1991 <liftingthedata@gmail.com>
2023-02-06 12:53:53 +0200

    31e83c1a87

--- components/dbt/app/models/bi_m2m_report/dbt__bi__yellow_m2m.sql
+++ components/dbt/app/models/bi_m2m_report/dbt__bi__yellow_m2m.sql
@@ -35,8 +35,9 @@ FROM {{ source("historical_data", "yellow_historical") }} bt
 left join {{ ref("seed_zones") }} z1 on bt.pickup_zone = z1.LocationID
 left join {{ ref("seed_zones") }} z2 on bt.dropoff_zone = z2.LocationID 
 WHERE z1.LocationID IS NOT NULL AND z2.LocationID IS NOT NULL
-AND COALESCE(average_passenger_count, 1) <= 7 AND
-AND year >= 2010 AND year <= EXTRACT(YEAR FROM CURRENT_DATE())
+AND COALESCE(average_passenger_count, 1) <= 7
+AND year >= 2010 
+AND year <= EXTRACT(YEAR FROM CURRENT_DATE())
 
 
 
