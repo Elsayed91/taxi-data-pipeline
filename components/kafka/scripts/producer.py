0489a7c1062eba7bade1c1a7095c1e3510250f47
Lestrang1991 <liftingthedata@gmail.com>
2023-02-09 13:28:12 +0200

    717f1fce95

--- components/kafka/scripts/producer.py
+++ components/kafka/scripts/producer.py
@@ -7,7 +7,10 @@ KAFKA_BROKER_URL = "localhost:9092"  # replace with your Kafka broker URL
 KAFKA_TOPIC = "nytaxi_topic"  # replace with your Kafka topic name
 PARQUET_URL = "https://d37ci6vzurychx.cloudfront.net/trip-data/yellow_tripdata_2022-11.parquet"  # replace with your Parquet URL
 
-producer = KafkaProducer(bootstrap_servers=KAFKA_BROKER_URL)
+producer = KafkaProducer(
+    bootstrap_servers=KAFKA_BROKER_URL,
+    value_serializer=lambda v: json.dumps(v).encode("utf-8"),
+)
 
 # read the data from the Parquet file
 df = pd.read_parquet(PARQUET_URL)
@@ -15,7 +18,8 @@ df = pd.read_parquet(PARQUET_URL)
 
 # stream the data one row at a time
 for index, row in df.iterrows():
-    producer.send(KAFKA_TOPIC, value=json.dumps(row.to_dict()).encode("utf-8"))
+    producer.send(KAFKA_TOPIC, value=row.to_dict())
     time.sleep(60)  # wait for one minute
 
 producer.flush()
+json.dumps(msg).encode("utf-8")
