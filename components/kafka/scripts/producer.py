094cc9d9105bd376e0c8b4a43147525cef4731b3
Lestrang1991 <liftingthedata@gmail.com>
2023-02-09 13:56:59 +0200

    1f20750357

--- components/kafka/scripts/producer.py
+++ components/kafka/scripts/producer.py
@@ -2,6 +2,15 @@ import pandas as pd
 import time
 from kafka import KafkaProducer
 import json
+import logging
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+ch = logging.StreamHandler()
+ch.setLevel(logging.DEBUG)
+formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+ch.setFormatter(formatter)
+logger.addHandler(ch)
 
 KAFKA_BROKER_URL = "localhost:9092"  # replace with your Kafka broker URL
 KAFKA_TOPIC = "nytaxi_topic"  # replace with your Kafka topic name
@@ -14,20 +23,21 @@ producer = KafkaProducer(
 
 # read the data from the Parquet file
 df = pd.read_parquet(PARQUET_URL)
-print(f"loaded dataframe (shape: {df.shape})into memory.")
+logger.info(f"loaded dataframe (shape: {df.shape})into memory.")
 df[["tpep_pickup_datetime", "tpep_dropoff_datetime"]] = df[
     ["tpep_pickup_datetime", "tpep_dropoff_datetime"]
 ].astype(
     str
 )  #
 
-print("Converted timestamps to string type due to serialization limitation.")
-print("Streaming the data.")
+logger.info("Converted timestamps to string type due to serialization limitation.")
+logger.info("Streaming the data.")
 # stream the data one row at a time
 for index, row in df.iterrows():
+    logger.info(f"{row.to_dict()} -> sent")
     producer.send(KAFKA_TOPIC, value=row.to_dict())
-    print(f"{row.to_dict()} -> sent")
-    time.sleep(60)  # wait for one minute
+    logger.info(f"{row.to_dict()} -> sent")
+    time.sleep(2)  # wait for one minute
 
 producer.flush()
 json.dumps(msg).encode("utf-8")
