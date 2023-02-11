a42fbf62671d8d2f29ec04ec5a1db6e33aec0636
Lestrang1991 <liftingthedata@gmail.com>
2023-02-11 03:43:55 +0200

    cc97ae0289

--- components/airflow/dags/addons/parse_state.py
+++ components/airflow/dags/addons/parse_state.py
@@ -27,10 +27,7 @@ def parse_spark_application(body) -> KubeResourceState:
 
 
 def parse_deployment(body) -> KubeResourceState:
-    if "status" not in body:
-        return KubeResourceState.Pending
-
-    conditions = body["status"].get("conditions", [])
+    conditions = body.get("status", {}).get("conditions", [])
     available_condition = next(
         (c for c in conditions if c["type"] == "Available"), None
     )
@@ -38,15 +35,10 @@ def parse_deployment(body) -> KubeResourceState:
         (c for c in conditions if c["type"] == "Progressing"), None
     )
 
-    if (
-        available_condition is not None
-        and available_condition["status"] == "True"
-        and progressing_condition is not None
-        and progressing_condition["status"] == "True"
-    ):
+    if available_condition is not None and available_condition["status"] == "True":
         return KubeResourceState.Succeeded
     elif available_condition is not None and available_condition["status"] == "False":
-        return KubeResourceState.Running
+        return KubeResourceState.Failed
     elif (
         progressing_condition is not None and progressing_condition["status"] == "True"
     ):
