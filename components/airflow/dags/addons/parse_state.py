6244c4007bfa76b5228689d05fde2ba6624842ff
Lestrang1991 <liftingthedata@gmail.com>
2023-02-11 03:36:49 +0200

    a765c3740f

--- components/airflow/dags/addons/parse_state.py
+++ components/airflow/dags/addons/parse_state.py
@@ -36,11 +36,15 @@ def parse_deployment(body) -> KubeResourceState:
         (c for c in conditions if c["type"] == "Progressing"), None
     )
 
-    if available_condition is not None:
-        if available_condition["status"] == "True":
-            return KubeResourceState.Succeeded
-        else:
-            return KubeResourceState.Failed
+    if (
+        available_condition is not None
+        and available_condition["status"] == "True"
+        and progressing_condition is not None
+        and progressing_condition["status"] == "True"
+    ):
+        return KubeResourceState.Succeeded
+    elif available_condition is not None and available_condition["status"] == "False":
+        return KubeResourceState.Running
     elif (
         progressing_condition is not None and progressing_condition["status"] == "True"
     ):
