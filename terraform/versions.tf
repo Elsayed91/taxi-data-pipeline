aa4bf2482bfb3b4f2709b5c124265f17d0ad2fc4
Lestrang1991 <liftingthedata@gmail.com>
2023-02-12 20:14:19 +0200

    825e8e75ee

--- terraform/versions.tf
+++ terraform/versions.tf
@@ -1,27 +1,27 @@
-terraform {
-  backend "gcs" {
-    bucket = "4e08a42ce7-bucket-tfstate"
-    prefix = "terraform/state"
-  }
-  required_providers {
-    local = {
-      source  = "hashicorp/local"
-      version = ">= 2.2.3"
-    }
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.45.0"
-    }
-    google = {
-      source  = "hashicorp/google"
-      version = "~> 4.45.0"
-    }
-    random = {
-      source = "hashicorp/random"
-    }
-  }
-  required_version = ">= 1.0"
-}
+# terraform {
+#   backend "gcs" {
+#     bucket = "4e08a42ce7-bucket-tfstate"
+#     prefix = "terraform/state"
+#   }
+#   required_providers {
+#     local = {
+#       source  = "hashicorp/local"
+#       version = ">= 2.2.3"
+#     }
+#     aws = {
+#       source  = "hashicorp/aws"
+#       version = "~> 4.45.0"
+#     }
+#     google = {
+#       source  = "hashicorp/google"
+#       version = "~> 4.45.0"
+#     }
+#     random = {
+#       source = "hashicorp/random"
+#     }
+#   }
+#   required_version = ">= 1.0"
+# }
 
 
 
