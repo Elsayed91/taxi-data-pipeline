16a66c6a324356f687778c55dc54e8343b1cea27
Lestrang1991 <liftingthedata@gmail.com>
2023-02-12 20:21:23 +0200

    a94da6842a

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,29 +2,27 @@
 locals {
   # sensitive_env = try({ for tuple in regexall("(.*?)=\"(.*)\"", file("${path.module}/../.env")) : tuple[0] => sensitive(tuple[1]) }, null)
   envs = { for tuple in regexall("(.*?)=(\"|)(.*)(\"|)", file("${var.env_path}")) : tuple[0] => tuple[2] }
-
-  # gcp  = yamldecode(templatefile("${path.module}/configuration/gcp.yaml", local.envs))
-  # aws  = yamldecode(templatefile("${path.module}/configuration/aws.yaml", local.envs))
+  #  (\"|) matches either " or nothing, and (\"|) matches either " or nothing. So the final
+  #  regex matches either (.*?)="(.*)" or (.*?)=(.*).
+  gcp = yamldecode(templatefile("${path.module}/configuration/gcp.yaml", local.envs))
+  aws = yamldecode(templatefile("${path.module}/configuration/aws.yaml", local.envs))
 }
 
-# module "project" {
-#   source           = "./modules/"
-#   project          = local.envs["PROJECT"]
-#   region           = local.envs["GCP_REGION"]
-#   location         = local.envs["GCP_ZONE"]
-#   custom_roles     = local.gcp["IAM"]["custom_roles"]
-#   service_accounts = local.gcp["IAM"]["service-accounts"]
-#   bigquery         = local.gcp["BIGQUERY"]["datasets"]
-#   gke-clusters     = local.gcp["GKE"]["clusters"]
-#   gke-node-pools   = local.gcp["GKE"]["node_pools"]
-#   gcs-buckets      = local.gcp["GCS"]["buckets"]
-#   gcs-files        = local.gcp["GCS"]["files"]
-#   s3-secrets       = local.aws["SECRETSMANAGER"]
-#   s3-buckets       = local.aws["S3"]
-#   lambda           = local.aws["LAMBDA"]
-# }
-
-
-output "envs" {
-  value = local.envs
+module "project" {
+  source           = "./modules/"
+  project          = local.envs["PROJECT"]
+  region           = local.envs["GCP_REGION"]
+  location         = local.envs["GCP_ZONE"]
+  custom_roles     = local.gcp["IAM"]["custom_roles"]
+  service_accounts = local.gcp["IAM"]["service-accounts"]
+  bigquery         = local.gcp["BIGQUERY"]["datasets"]
+  gke-clusters     = local.gcp["GKE"]["clusters"]
+  gke-node-pools   = local.gcp["GKE"]["node_pools"]
+  gcs-buckets      = local.gcp["GCS"]["buckets"]
+  gcs-files        = local.gcp["GCS"]["files"]
+  s3-secrets       = local.aws["SECRETSMANAGER"]
+  s3-buckets       = local.aws["S3"]
+  lambda           = local.aws["LAMBDA"]
 }
+
+
--- terraform/providers.tf
+++ terraform/providers.tf
@@ -1,11 +1,11 @@
-# provider "google" {
-#   project = local.envs["PROJECT"]
-#   region  = local.envs["GCP_REGION"]
-#   zone    = local.envs["GCP_ZONE"]
-# }
+provider "google" {
+  project = local.envs["PROJECT"]
+  region  = local.envs["GCP_REGION"]
+  zone    = local.envs["GCP_ZONE"]
+}
 
-# provider "aws" {
-#   access_key = sensitive(local.envs["AWS_ACCESS_KEY_ID"])
-#   secret_key = sensitive(local.envs["AWS_SECRET_ACCESS_KEY"])
-#   region     = local.envs["AWS_REGION"]
-# }
+provider "aws" {
+  access_key = sensitive(local.envs["AWS_ACCESS_KEY_ID"])
+  secret_key = sensitive(local.envs["AWS_SECRET_ACCESS_KEY"])
+  region     = local.envs["AWS_REGION"]
+}
