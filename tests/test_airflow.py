e6aeac1016260db9415dd79a8d130e1ac9e25a40
Lestrang1991 <liftingthedata@gmail.com>
2023-02-13 01:37:17 +0200

    fbfe6fab88

--- tests/test_airflow.py
+++ tests/test_airflow.py
@@ -2,12 +2,12 @@ from airflow.models import DagBag
 
 
 def test_no_import_errors():
-    dag_bag = DagBag()
+    dag_bag = DagBag(dag_folder="components/airflow/dags", include_examples=False)
     assert len(dag_bag.import_errors) == 0, "No Import Failures"
 
 
 def test_retries_present():
-    dag_bag = DagBag()
+    dag_bag = DagBag(dag_folder="components/airflow/dags", include_examples=False)
     for dag in dag_bag.dags:
         retries = dag_bag.dags[dag].default_args.get("retries", [])
         error_msg = f"Retries not set to 0 for DAG {dag}"
@@ -15,7 +15,7 @@ def test_retries_present():
 
 
 def test_in_cluster():
-    dag_bag = DagBag()
+    dag_bag = DagBag(dag_folder="components/airflow/dags", include_examples=False)
     for dag in dag_bag.dags:
         in_cluster = dag_bag.dags[dag].default_args.get("in_cluster", [])
         error_msg = f"in_cluster not set for DAG {dag}"
